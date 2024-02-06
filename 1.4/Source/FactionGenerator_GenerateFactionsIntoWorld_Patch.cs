7821d40feb1466d085342539d0429c9a625120b3
Taranchuk <nfhfyxer@gmail.com>
2024-02-06 14:49:47 +0200

    mod

---
+++ 1.4/Source/FactionGenerator_GenerateFactionsIntoWorld_Patch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using RimWorld;
+
+namespace ImperialColonization
+{
+    [HarmonyPatch(typeof(FactionGenerator), "GenerateFactionsIntoWorld")]
+    public static class FactionGenerator_GenerateFactionsIntoWorld_Patch
+    {
+        public static void Prefix()
+        {
+            FactionDefOf.Empire.hidden = true;
+        }
+        public static void Postfix()
+        {
+            FactionDefOf.Empire.hidden = false;
+        }
+    }
+}
---
+++ 1.4/Source/QuestPart_TrackShipBeacons.cs
@@ -0,0 +1,30 @@
+﻿using RimWorld;
+using RimWorld.Planet;
+using Verse;
+
+namespace ImperialColonization
+{
+    public class QuestPart_TrackShipBeacons : QuestPartActivable
+    {
+        public MapParent parent;
+        public override void QuestPartTick()
+        {
+            base.QuestPartTick();
+            if (parent.HasMap)
+            {
+                foreach (var beacon in parent.Map.listerThings.ThingsOfDef(ThingDefOf.ShipLandingBeacon))
+                {
+                    var comp = beacon.TryGetComp<CompShipLandingBeacon>();
+                    foreach (var area in comp.LandingAreas)
+                    {
+                        if (area.Clear && area.Active)
+                        {
+                            QuestUtility.SendQuestTargetSignals(parent.questTags, "ShipLandingBeaconSetUp", parent.Named("SUBJECT"));
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
